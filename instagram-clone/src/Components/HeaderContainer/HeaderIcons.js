c5a70780ce8b0810e1b37cc20898629815c18929
David <n.dave.hernandez@gmail.com>
2019-12-20 04:52:49 -0500

    Header Icons

--- instagram-clone/src/Components/HeaderContainer/HeaderIcons.js
+++ instagram-clone/src/Components/HeaderContainer/HeaderIcons.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faCompass } from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const HeaderIconStyles = styled.div`
+  display: flex;
+  align-items: baseline;
+  height: auto;
+`;
+
+const Icon = styled.div`
+  font-size: 40px;
+  padding: 15px;
+  }
+`;
 
 function HeaderIcons() {
   return (
-    <div>
-      <p>Column 3</p>
-    </div>
+    <HeaderIconStyles>
+      <Icon>
+        <FontAwesomeIcon icon={faCompass} />
+        <FontAwesomeIcon icon={faHeart} />
+        <FontAwesomeIcon icon={faUser} />
+      </Icon>
+    </HeaderIconStyles>
   );
 }
 
